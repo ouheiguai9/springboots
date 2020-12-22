10ad54a7d4bcf560e0e39c824c85badf8d135436
ouheiguai9 <349382440@qq.com>
2020-12-22 15:28:02 +0800

    添加数据初始化

---
+++ factory/src/main/resources/sql/data.sql
@@ -0,0 +1,4 @@
+/*初始化角色*/
+INSERT INTO `t_sys_role` VALUES ('init_data_role_00001', '2020-12-22 10:37:09', '2020-12-22 12:54:21', b'0', '系统管理员', '系统管理员', NULL, NULL);
+INSERT INTO `t_sys_role` VALUES ('init_data_role_00002', '2020-12-22 12:53:57', '2020-12-22 12:53:57', b'0', '工厂老板', '工厂老板', NULL, NULL);
+INSERT INTO `t_sys_role` VALUES ('init_data_role_00003', '2020-12-22 12:54:18', '2020-12-22 12:54:18', b'0', '租赁厂老板', '租赁厂老板', NULL, NULL);
