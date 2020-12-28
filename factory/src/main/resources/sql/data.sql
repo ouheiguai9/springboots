7b9088245a2c540e3d74c113be7173e3581c8661
ouheiguai9 <349382440@qq.com>
2020-12-28 11:42:33 +0800

    增加工厂管理部分菜单

--- factory/src/main/resources/sql/data.sql
+++ factory/src/main/resources/sql/data.sql
@@ -13,3 +13,9 @@ INSERT INTO `t_sys_menu` VALUES ('init_data_menu_00002', '2020-12-22 16:15:11', 
 INSERT INTO `t_sys_menu` VALUES ('init_data_menu_00003', '2020-12-22 16:15:11', '2020-12-22 16:15:11', b'0', 'role', '系统核心功能请勿随意修改和授权', 'layui-icon-group', '角色管理', 1, NULL, NULL, 'init_data_menu_00001');
 INSERT INTO `t_sys_menu` VALUES ('init_data_menu_00004', '2020-12-22 16:15:11', '2020-12-22 16:15:11', b'0', 'menu', '系统核心功能请勿随意修改和授权', 'layui-icon-menu-fill', '菜单管理', 2, NULL, NULL, 'init_data_menu_00001');
 INSERT INTO `t_sys_menu` VALUES ('init_data_menu_00005', '2020-12-22 16:15:11', '2020-12-22 16:15:11', b'0', 'device', '系统核心功能请勿随意修改和授权', 'layui-icon-component', '设备管理', 3, NULL, NULL, 'init_data_menu_00001');
+
+INSERT INTO `t_sys_menu` VALUES ('init_data_menu_00101', '2020-12-22 16:10:34', '2020-12-22 16:10:34', b'0', 'factory', '系统核心功能请勿随意修改和授权', 'layui-icon-home', '设备工厂', 200, NULL, NULL, NULL);
+INSERT INTO `t_sys_menu` VALUES ('init_data_menu_00102', '2020-12-22 16:15:11', '2020-12-22 16:15:11', b'0', 'factoryAnalysis', '系统核心功能请勿随意修改和授权', 'layui-icon-chart-screen', '效率分析', 101, NULL, NULL, 'init_data_menu_00101');
+INSERT INTO `t_sys_menu` VALUES ('init_data_menu_00103', '2020-12-22 16:15:11', '2020-12-22 16:15:11', b'0', 'factoryMachine', '系统核心功能请勿随意修改和授权', 'layui-icon-templeate-1', '设备管理', 102, NULL, NULL, 'init_data_menu_00101');
+INSERT INTO `t_sys_menu` VALUES ('init_data_menu_00104', '2020-12-22 16:15:11', '2020-12-22 16:15:11', b'0', 'factorySchedual', '系统核心功能请勿随意修改和授权', 'layui-icon-tabs', '班次管理', 103, NULL, NULL, 'init_data_menu_00101');
+INSERT INTO `t_sys_menu` VALUES ('init_data_menu_00105', '2020-12-22 16:15:11', '2020-12-22 16:15:11', b'0', 'factoryView', '系统核心功能请勿随意修改和授权', 'layui-icon-console', '全厂总览', 104, NULL, NULL, 'init_data_menu_00101');
