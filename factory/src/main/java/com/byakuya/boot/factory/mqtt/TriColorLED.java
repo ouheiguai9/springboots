79b746bd6ff2e4ea44c938e69236f79fd19cb303
ouheiguai9 <349382440@qq.com>
2021-01-04 15:32:24 +0800

    mqtt消息转码

--- factory/src/main/java/com/byakuya/boot/factory/mqtt/TriColorLED.java
+++ factory/src/main/java/com/byakuya/boot/factory/mqtt/TriColorLED.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+
 /**
  * Created by ganzl on 2020/12/30.
  */
@@ -12,7 +14,17 @@ import org.springframework.stereotype.Component;
 public class TriColorLED {
     @JmsListener(destination = "TriColorLED.status")
     public void subscriber(String text) {
-        log.warn("三色灯状态:" + text);
+        String status = asciiToString(text);
+        log.warn("三色灯状态:" + status);
+    }
+
+    private static String asciiToString(String value) {
+        StringBuilder sbu = new StringBuilder();
+        String[] chars = value.split(",");
+        for (String aChar : chars) {
+            sbu.append((char) Integer.parseInt(aChar));
+        }
+        return sbu.toString();
     }
 
 }
