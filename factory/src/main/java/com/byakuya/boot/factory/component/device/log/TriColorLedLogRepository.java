57f15ebed169297228be751a737b37564db8a9b3
ouheiguai9 <349382440@qq.com>
2021-01-13 18:07:04 +0800

    枚举更换为枚举字符串

--- factory/src/main/java/com/byakuya/boot/factory/component/device/log/TriColorLedLogRepository.java
+++ factory/src/main/java/com/byakuya/boot/factory/component/device/log/TriColorLedLogRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
  * Created by ganzl on 2021/1/5.
  */
 public interface TriColorLedLogRepository extends CrudRepository<TriColorLedLog, String> {
-    @Query("select new TriColorLedLog(log.device, log.status, sum(log.duration)) from TriColorLedLog log where log.status <> TriColorLedLog.Status.NONE and log.device in ?1 and log.time between ?2 and ?3 group by log.status, log.device")
+    @Query("select new TriColorLedLog(log.device, log.status, sum(log.duration)) from TriColorLedLog log where log.status <> 'NONE' and log.device in ?1 and log.time between ?2 and ?3 group by log.status, log.device")
     List<TriColorLedLog> findStatusRank(Iterable<Device> devices, LocalDateTime startTime, LocalDateTime endTime);
 
     Optional<TriColorLedLog> findFirstByDevice_IdOrderByTimeDesc(String deviceId);
