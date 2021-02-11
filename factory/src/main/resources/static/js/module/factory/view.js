b3053660caa48ef4e7f949d415da22a6353a43ac
ouheiguai9 <349382440@qq.com>
2021-02-11 16:29:34 +0800

    时间轴开始修改为确定日期

--- factory/src/main/resources/static/js/module/factory/view.js
+++ factory/src/main/resources/static/js/module/factory/view.js
@@ -15,6 +15,22 @@ layui.config({
   renderTable();
 
   /********************************事件绑定*********************************/
+  Date.prototype.format = function (fmt) { //author: meizz
+    var o = {
+      'M+': this.getMonth() + 1, //月份
+      'd+': this.getDate(), //日
+      'h+': this.getHours(), //小时
+      'm+': this.getMinutes(), //分
+      's+': this.getSeconds(), //秒
+      'q+': Math.floor((this.getMonth() + 3) / 3), //季度
+      'S': this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    for (var k in o)
+      if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+    return fmt;
+  }
+
   function renderTable() {
     $('div.layui-card').toggleClass('layui-hide');
     $('#singleCard').empty();
@@ -236,7 +252,11 @@ layui.config({
         scale: true,
         axisLabel: {
           formatter: function (val) {
-            return Math.max(0, val - startTime) + ' s';
+            if (val > startTime) {
+              return (val - startTime) + ' s';
+            } else {
+              return (new Date(val * 1000)).format('yyyy-MM-dd hh:mm:ss');
+            }
           }
         }
       },
