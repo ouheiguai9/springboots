e8378ec29976f2cfe3a8faf491bd29fbfbfc47c4
ouheiguai9 <349382440@qq.com>
2021-02-20 11:22:06 +0800

    增加密码找回提示

--- factory/src/main/resources/static/js/login.js
+++ factory/src/main/resources/static/js/login.js
@@ -1,6 +1,7 @@
 ;
-layui.use(['form', 'jquery'], function () {
+layui.use(['form', 'jquery', 'layer'], function () {
   var $ = layui.$;
+  var layer = layui.layer;
 
   /********************************组件渲染*********************************/
 
@@ -11,5 +12,8 @@ layui.use(['form', 'jquery'], function () {
   });
   $('img.layui-admin-login-captcha').on('click', function () {
     $(this).prop('src', 'captcha?r=' + Math.random());
+  });
+  $('#btnForgetPassword').on('click', function () {
+    layer.alert('尊敬的企业客户：<br/>如果密码丢失，请联系人工客服。<br/>联系电话：18114758636/0512-68052881');
   });
 });
